3e46d49dea2a87a8023049885384207ed788f304
Matthias Mandelartz <33543464+GHMatti@users.noreply.github.com>
2018-10-12 09:48:06 +0200

    Added maxNumberOfAccounts clientside

--- src/ui/src/store.js
+++ src/ui/src/store.js
@@ -16,6 +16,7 @@ export default new Vuex.Store({
     config: {
       locale: 'en-US',
       currency: 'USD',
+      maxNumberOfAccounts: 3,
     },
     // functionality
     display: 'account-select',
